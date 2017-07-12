41a0f8811879d868a1c0e87e6ac8d686cccd7a66
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-07-12 14:05:00 +0530

    Removed redundant function call

--- bank/statements/v1/views.py
+++ bank/statements/v1/views.py
@@ -12,12 +12,11 @@ LOGGER = logging.getLogger(__name__)
 
 class StatementAnalyserDetails(APIView):
 
-    # @catch_exception(LOGGER)
+    @catch_exception(LOGGER)
     @meta_data_response()
     @iam('admin')
     def post(self, request, *args, **kwargs):
         serializer = serializers.StatementAnalyserSerializer(data=request.data)
         if serializer.is_valid() and kwargs.get('user_name_auth'):
-            serializer.bank_data()
             return Response(serializer.bank_data(), status=status.HTTP_200_OK)
         return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
