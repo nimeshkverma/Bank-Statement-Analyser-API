fd0d075d12233113c50f2daab0f6909b600f07ca
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-08-24 11:33:24 +0530

    Removed the Bugs

--- bank/statements/v1/services/Kotak_bank_statements_c_service.py
+++ bank/statements/v1/services/Kotak_bank_statements_c_service.py
@@ -2,8 +2,8 @@ import re
 import datetime
 from copy import deepcopy
 
-MIN_COLUMNS = 8
-MAX_COLUMNS = 8
+MIN_COLUMNS = 7
+MAX_COLUMNS = 9
 
 HEADER = set(['Sl. No.', 'Date', 'Description', 'Chq / Ref number',
               'Amount', 'Dr / Cr', 'Balance', 'Dr / Cr'])
