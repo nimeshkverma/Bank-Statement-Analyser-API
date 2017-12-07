16979f02dbf53c592c4b9b65511c0f42e701492c
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-12-07 11:09:28 +0530

    Email SMS and FCM service

--- bank/notification/v1/services/notification_fcm_constants.py
+++ bank/notification/v1/services/notification_fcm_constants.py
@@ -1,6 +1,46 @@
 NOTIFICATION_FCM_TYPE_DATA = {
-    'default': {
-        'data_message': 'This is data message from {msg}',
-        'message_body': 'This is data message from {body}',
+    'follow_up': {
+        'data_message': "Dear {full_name}, gentle reminder to complete your pending loan application on Upwards. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries. :)",
+        'message_body': "Dear {full_name}, gentle reminder to complete your pending loan application on Upwards. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries. :)",
+    },
+    'eligibility_documents_uploaded': {
+        'data_message': "Dear {full_name}, your documents for eligibility assessment are successfully uploaded on Upwards app :) Our team will get back to you within 48 hours with an approve/reject decision. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries.",
+        'message_body': "Dear {full_name}, your documents for eligibility assessment are successfully uploaded on Upwards app :) Our team will get back to you within 48 hours with an approve/reject decision. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries."
+    },
+    'bank_statement_incomplete': {
+        'data_message': "Dear {full_name}, your loan processing on Upwards is paused as bank statement provided by you is not complete. Please check your email for more details. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries.",
+        'message_body': "Dear {full_name}, your loan processing on Upwards is paused as bank statement provided by you is not complete. Please check your email for more details. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries."
+    },
+    'eligibility_evaluation_submit': {
+        'data_message': "Dear {full_name}, your eligibility section is successfully completed on Upwards app :) Our team will get back to you within 48 hours with an approve/reject decision. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries.",
+        'message_body': "Dear {full_name}, your eligibility section is successfully completed on Upwards app :) Our team will get back to you within 48 hours with an approve/reject decision. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries."
+    },
+    'eligibility_evaluation_approved': {
+        'data_message': "Dear {full_name}, we are pleased to inform you that your loan application is approved. Kindly complete the next KYC section on our app to get your loan amount disbursed.",
+        'message_body': "Dear {full_name}, we are pleased to inform you that your loan application is approved. Kindly complete the next KYC section on our app to get your loan amount disbursed."
+    },
+    'eligibility_evaluation_reject': {
+        'data_message': "Dear {full_name}, we regret to inform that your loan application is rejected. Please check your email for more details. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries.",
+        'message_body': "Dear {full_name}, we regret to inform that your loan application is rejected. Please check your email for more details. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries."
+    },
+    'post_eligibility_follow_up': {
+        'data_message': "Dear {full_name}, we are pleased to inform you that your loan application is approved. Kindly complete the next KYC section on our app to get your loan amount disbursed. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries. ",
+        'message_body': "Dear {full_name}, we are pleased to inform you that your loan application is approved. Kindly complete the next KYC section on our app to get your loan amount disbursed. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries. "
+    },
+    'kyc_documents_uploaded': {
+        'data_message': "Dear {full_name}, your documents for KYC assessment are successfully uploaded on Upwards app :) Our team will get back to you within 24-48 hours with next steps. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries ",
+        'message_body': "Dear {full_name}, your documents for KYC assessment are successfully uploaded on Upwards app :) Our team will get back to you within 24-48 hours with next steps. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries "
+    },
+    'kyc_submit': {
+        'data_message': "Dear {full_name}, your KYC section is successfully completed on Upwards app :) Our team will get back to you within 24-48 hours with next steps. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries.",
+        'message_body': "Dear {full_name}, your KYC section is successfully completed on Upwards app :) Our team will get back to you within 24-48 hours with next steps. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries."
+    },
+    'kyc_approved': {
+        'data_message': "Dear {full_name}, we are pleased to inform you that your loan application is approved. Kindly complete the next KYC section on our app to get your loan amount disbursed. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries.",
+        'message_body': "Dear {full_name}, we are pleased to inform you that your loan application is approved. Kindly complete the next KYC section on our app to get your loan amount disbursed. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries. "
+    },
+    'kyc_reject': {
+        'data_message': "Dear {full_name}, we regret to inform that your loan application is rejected. Please check your email for more details. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries.",
+        'message_body': "Dear {full_name}, we regret to inform that your loan application is rejected. Please check your email for more details. Please feel free to reach us on help@go-upwards.com or on our app's helpchat for any queries."
     }
 }
--- bank/notification/v1/services/notification_sms_constants.py
+++ bank/notification/v1/services/notification_sms_constants.py
@@ -1,7 +1,37 @@
 DEFAULT_SENDER = 'UPWARD'
 
 NOTIFICATION_SMS_TYPE_DATA = {
-    'default': {
-        'template': 'UPWARD_test',
+    'follow_up': {
+        'template': 'follow_up_sms',
+    },
+    'eligibility_documents_uploaded': {
+        'template': 'eligibility_documents_uploaded_sms',
+    },
+    'bank_statement_incomplete': {
+        'template': 'bank_statement_incomplete_sms',
+    },
+    'eligibility_evaluation_submit': {
+        'template': 'eligibility_evaluation_submit_sms',
+    },
+    'eligibility_evaluation_approved': {
+        'template': 'eligibility_evaluation_approved_sms',
+    },
+    'eligibility_evaluation_reject': {
+        'template': 'eligibility_evaluation_reject_sms',
+    },
+    'post_eligibility_follow_up': {
+        'template': 'post_eligibility_follow_up_sms',
+    },
+    'kyc_documents_uploaded': {
+        'template': 'kyc_documents_uploaded_sms',
+    },
+    'kyc_submit': {
+        'template': 'kyc_submit_sms',
+    },
+    'kyc_approved': {
+        'template': 'kyc_approved_sms',
+    },
+    'kyc_reject': {
+        'template': 'kyc_reject_sms',
     }
 }
